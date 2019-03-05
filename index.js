93ba543b80f281f24be6edccf3df2cbee52499f3
shevchuk11 <v.shevchuk.1105@gmail.com>
2019-03-05 13:02:29 +0200

    2
    2

--- index.js
+++ index.js
@@ -1,19 +1,38 @@
 var second = 234245645335 ;
-var hour = second / 3600;
-console.log("Hours "+hour);
-var ost = second % 3600 ;
-console.log("seconds "+ost);
+var hour1 = Math.floor(second / 3600);
+console.log(hour1);
+var seconde =second % 3600;
+console.log(seconde);
+var minut = Math.floor(seconde / 60);
+console.log(minut);
+var hour = Math.floor(seconde % 60);
+console.log(hour);
 
 
 
 var str = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, at.";
 
-var h = [];
+var m = [];
 
-for (i=0; i<str.length; i++){
+for (i=1; i<str.length; i++){
 	if (str.charAt(i) == "a"){
-		h.push(i);
+		m.push(i);
 	}
 }
+var x = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, at.";
 
-console.log(h);
+while (x.indexOf("a", i ) != -1){ 
+	console.log(x.indexOf("a",i)); 
+	i++;
+}
+
+var d = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam voluptatibus magni natus ad quibusdam, obcaecati deserunt incidunt voluptatum odio sit ipsum explicabo, sequi dolorem aliquid veniam, error reprehenderit placeat odit perspiciatis non excepturi. Aut itaque ut quibusdam earum nulla cum perferendis mollitia magni sit asperiores nam quod soluta, odio, vel in, doloribus unde. Reprehenderit aliquam nam ipsam molestiae, vel nulla dolorem cupiditate dicta cum, ad temporibus enim. Eius voluptatibus, autem dolore debitis ipsam cumque aliquid porro maxime distinctio iste aperiam, vel minima facilis optio voluptates fugit modi quas pariatur? Illum cum fuga ut quasi accusamus perferendis earum repellat quas optio.";
+ for (var i = 1; i<d.length; i++){
+ 	
+ 	console.log(x.charAt(i));
+ }
+ // for (var i of d){
+ // 	console.log(i);
+ // }
+
+console.log(m);
